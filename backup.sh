d03ae74c26688d0f70d6f392845a12404d7ce65b
Robert Trzebinski <robert@trzebinski.info>
2024-02-13 00:28:55 +0100

    whitelist

--- backup.sh
+++ backup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 mkdir -p /Volumes/T7\ Shield/minecraft_backup
 cp -R ~/minecraft-data/worlds /Volumes/T7\ Shield/minecraft_backup/$(date +'%Y-%m-%d_%H-%M-%S') && date >~/cron.log 2>~/cron.log
