ba2712a31e81b1c3e8e27c3cef5c50b40687bf3b
Robert Trzebinski <robert@trzebinski.info>
2025-01-04 09:59:28 +0100

    wait for pod to be ready

--- restart.sh
+++ restart.sh
@@ -2,4 +2,6 @@
 /usr/local/bin/kubectx docker-desktop
 cd /Users/robert/projects/minecraft-bedrock-server-kubernetes
 /usr/bin/make restart
+/usr/local/bin/kubectl wait --for=condition=Ready pod/bds-0 -n minecraft-server --timeout=300s
+/usr/bin/make config
 echo "completed" `date`
