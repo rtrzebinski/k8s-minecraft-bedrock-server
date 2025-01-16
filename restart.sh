285795297be1c1d8e1fd19c10df99f3fdcaecfee
Robert Trzebinski <robert@trzebinski.info>
2025-01-16 08:30:53 +0100

    fix restart + config with bash aliases

--- restart.sh
+++ restart.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-/usr/local/bin/kubectx docker-desktop
+
+alias make='/usr/bin/make'
+alias kubectl='/usr/local/bin/kubectl'
+alias kubectx='/usr/local/bin/kubectx'
+
+kubectx docker-desktop
+
 cd /Users/robert/projects/minecraft-bedrock-server-kubernetes
-/usr/bin/make restart
-/usr/local/bin/kubectl wait --for=condition=Ready pod/bds-0 -n minecraft-server --timeout=300s
-/usr/bin/make config
+
+make restart
+
+kubectl wait --for=condition=Ready pod/bds-0 -n minecraft-server --timeout=60s
+
+make config
+
 echo "completed" `date`
